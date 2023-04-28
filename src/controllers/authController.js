238aaaf805c3464ec3b80b75b95efeeccbc2a7bd
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-04-29 01:16:44 +0530

    Add basic user registraion function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,33 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const fs = require("fs");
+const path = require("path");
 
-const login = (req, res) => {
-  return true;
-};
+const usersFile = path.join(__dirname, "..", "users.json");
+
+function readUsers() {
+  const data = fs.readFileSync(usersFile);
+  return JSON.parse(data);
+}
+
+function writeUsers(user) {
+  const data = JSON.stringify(user);
+  return fs.writeFileSync(usersFile, data);
+}
 
 const register = (req, res) => {
-  return true;
+  const { id, username, password } = req.body;
+  const users = readUsers();
+  const existingUser = users.find((user) => user.username === username);
+  if (existingUser) res.status(400).json({ message: "User already exists" });
+  const newUser = { id, username, password };
+  users.push(newUser);
+  writeUsers(users);
+  return res.status(201).json({ message: "User registered successfully." });
+};
+
+const login = (req, res) => {
+  const { username, password } = req.body;
+  const users = readUsers();
+  return res.status(200).json({ message: "boilerplate" });
 };
 
 module.exports = { login, register };
