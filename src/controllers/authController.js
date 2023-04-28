55dc5aebfa2fa6b7bb9407bf7584ac3aab6419b2
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-04-29 01:36:54 +0530

    Validate body using `ajv`

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,21 @@
+const Ajv = require("ajv");
 const fs = require("fs");
 const path = require("path");
 
+const ajv = new Ajv();
+
 const usersFile = path.join(__dirname, "..", "users.json");
+
+const userSchema = {
+  type: "object",
+  properties: {
+    id: { type: "string", minLength: 1 },
+    username: { type: "string", minLength: 3 },
+    password: { type: "string", minLength: 3 },
+  },
+  required: ["id", "username", "password"],
+  additionalProperties: false,
+};
 
 function readUsers() {
   const data = fs.readFileSync(usersFile);
@@ -14,14 +28,22 @@ function writeUsers(user) {
 }
 
 const register = (req, res) => {
+  const validBody = ajv.validate(userSchema, req.body);
   const { id, username, password } = req.body;
-  const users = readUsers();
-  const existingUser = users.find((user) => user.username === username);
-  if (existingUser) res.status(400).json({ message: "User already exists" });
-  const newUser = { id, username, password };
-  users.push(newUser);
-  writeUsers(users);
-  return res.status(201).json({ message: "User registered successfully." });
+  if (validBody) {
+    const users = readUsers();
+    const existingUser = users.find((user) => user.username === username);
+    if (existingUser) {
+      res.status(400).json({ message: "User already exists" });
+    } else {
+      const newUser = { id, username, password };
+      users.push(newUser);
+      writeUsers(users);
+      return res.status(201).json({ message: "User registered successfully." });
+    }
+  } else {
+    return res.status(400).json({ message: "Invalid user data" });
+  }
 };
 
 const login = (req, res) => {
