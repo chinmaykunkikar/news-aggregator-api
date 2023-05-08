6d4cae1e5f69a6410b727428590fa79f60b41e4e
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-05-08 20:51:05 +0530

    Revert "Use `express.csrf()` middleware"
    This reverts commit e14f155045f5c948dc3683afb70aa22b6a7071f3.

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ const PORT = 3000;
 const app = express();
 
 app.use(helmet());
-app.use(express.csrf());
 app.use(routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
