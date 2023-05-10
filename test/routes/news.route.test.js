a83b672fbd06dbd2fa09b1f8c106de1f0c020734
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-05-11 04:09:47 +0530

    Tests: Nest tests under different `describe()` blocks

--- test/routes/news.route.test.js
+++ test/routes/news.route.test.js
@@ -7,63 +7,79 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe("News APIs", () => {
-  before((done) => {
-    chai
-      .request(app)
-      .post("/login")
-      .send({ username: "foo", password: "bar" })
-      .end((err, res) => {
-        expect(err).to.be.null;
-        token = res.body.token;
-        done();
-      });
+  describe("/news endpoint", () => {
+    before((done) => {
+      chai
+        .request(app)
+        .post("/login")
+        .send({ username: "foo", password: "bar" })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          token = res.body.token;
+          done();
+        });
+    });
+
+    it("should return news articles", (done) => {
+      chai
+        .request(app)
+        .get("/news")
+        .set("Authorization", `JWT ${token}`)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("array");
+          done();
+        });
+    });
+
+    it("should not return news articles without auth", (done) => {
+      chai
+        .request(app)
+        .get("/news")
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(401);
+          done();
+        });
+    });
   });
 
-  it("should return articles from /everything endpoint", (done) => {
-    chai
-      .request(app)
-      .get("/news")
-      .set("Authorization", `JWT ${token}`)
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an("array");
-        done();
-      });
-  });
+  describe("/news/top endpoint", () => {
+    before((done) => {
+      chai
+        .request(app)
+        .post("/login")
+        .send({ username: "foo", password: "bar" })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          token = res.body.token;
+          done();
+        });
+    });
 
-  it("should return articles from /top-headlines endpoint", (done) => {
-    chai
-      .request(app)
-      .get("/news/top")
-      .set("Authorization", `JWT ${token}`)
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an("array");
-        done();
-      });
-  });
+    it("should return news articles", (done) => {
+      chai
+        .request(app)
+        .get("/news/top")
+        .set("Authorization", `JWT ${token}`)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("array");
+          done();
+        });
+    });
 
-  it("should not return /news without auth", (done) => {
-    chai
-      .request(app)
-      .get("/news")
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(401);
-        done();
-      });
-  });
-
-  it("should not return /news/top without auth", (done) => {
-    chai
-      .request(app)
-      .get("/news/top")
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(401);
-        done();
-      });
+    it("should not return news articles without auth", (done) => {
+      chai
+        .request(app)
+        .get("/news/top")
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(401);
+          done();
+        });
+    });
   });
 });
